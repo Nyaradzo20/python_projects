12d2327e1e2fd06004815ac6d535248102d95d77
Nyaradzo20 <nchinyati20@gmail.com>
2022-08-12 17:09:50 -0700

    madlibs game

---
+++ madlibs/README.md
@@ -0,0 +1,5 @@
+**************************************************************************************
+-made a madlibs  game using python
+-covered  string concatination
+-""""""""""""""""variables
+-the input funtion & the print function
---
+++ madlibs/madlibs.py
@@ -0,0 +1,15 @@
+#string concatenation(how to put strings together)
+# we want to create a stering thats says 'hi i am ____"
+#name = "Dora"
+#ways to do this
+'''
+print('hi i am ' + name)
+print('hi i am {}'.format(name))
+print(f"hi i am {name}")
+'''
+adje =  input("Write adjective: ")
+verb1 = input("write a verb: ")
+verb2 = input("Write a verb: ")
+famous_person = input("famous person: ")
+madlib = f"Computer programming is so {adje}! It makes me so excited all the time because l love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
